4392d1ce36f481aba89b8727bb48bc97efa53044
JDAguirre <j1927aguirre@gmail.com>
2025-06-07 20:47:06 -0500

    feat: :sparkles: add global validation pipe for DTO's and set required fields for CreateProductDto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global prefix
+  app.setGlobalPrefix('api');
+  // Setting Global Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Removes the extra fields sent throughout the request
+      // Just keeps the ones on the DTO
+      whitelist: true,
+      // Emits an error when there are extra fields on a request
+      // Indicates which property is not part of the DTO
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        // Excludes fields that are undefined on the request
+        exposeUnsetFields: false,
+        // Enables implicit conversion for DTO's
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
