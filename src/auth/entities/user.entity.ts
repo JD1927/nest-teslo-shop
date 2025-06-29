ced38eb8ad43b70fed8c40fcad86630d2a362b06
Juan Aguirre <j1927aguirre@gmail.com>
2025-06-29 10:46:42 -0500

    feat: :sparkles: implement authentication module with user entity, DTOs, and service methods

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true, type: 'text' })
+  email: string;
+
+  @Column({ type: 'text' })
+  password: string;
+
+  @Column({ type: 'text' })
+  fullName: string;
+
+  @Column({ type: 'bool', default: true })
+  isActive: boolean;
+
+  @Column({ type: 'text', array: true, default: ['user'] })
+  roles: string[];
+}
